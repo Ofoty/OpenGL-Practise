5c4073cd9cf6f55658d8afea6174263bea45bf51
Ofoty <wawlikk@mail.ru>
2020-11-17 15:14:41 +0500

    practise with uniform, color changing animation created

--- src/main.cpp
+++ src/main.cpp
@@ -128,6 +128,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if(glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
 
@@ -163,6 +165,13 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.8f, 0.8f, 1.0f, 1.0f));
+
+    float bColor = 0.0f;
+
+    float increment = 0.01f;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -170,7 +179,20 @@ int main(void)
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        GLCall(glUniform4f(location, 0.5f, bColor, 1.0f, 1.0f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        if(bColor > 1.0f)
+        {
+            increment = -0.01f;
+        }
+        else if(bColor < 0.0f)
+        {
+            increment = 0.01f;
+        }
+
+        bColor += increment;
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
